57755b386a837279ee589212fcf171466609ba8f
Yash Khadse <yashkhadse@gmail.com>
2023-07-22 22:09:19 +0530

    Add Delay in Testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,7 @@ import pytest
 from threadspy import ThreadsApi
 from dotenv import load_dotenv
 import os
+import time
 
 @pytest.fixture(scope="session")
 def threads_api():
@@ -9,3 +10,8 @@ def threads_api():
     load_dotenv()
     
     return ThreadsApi(username=os.getenv('IG_USERNAME'), password=os.getenv('IG_PASSWORD'))
+
+@pytest.fixture(autouse=True)
+def slow_down_tests():
+    yield
+    time.sleep(5)
